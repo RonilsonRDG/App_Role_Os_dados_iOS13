3a9fea64876e52da05ea98a1939204076e10acfe
Ronilson Rodrigues <g.ronilsongomes@gmail.com>
2023-04-17 23:00:22 -0300

    Adicionado controle do botao Rolar os Dados

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //IBOutlet permite fazer referencias a um elemtno de design do meu Storyboard
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var leftController: UISwitch!
+    @IBOutlet weak var rightController: UISwitch!
+    @IBOutlet weak var allController: UISwitch!
+    @IBOutlet weak var rollButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +24,68 @@ class ViewController: UIViewController {
         //Permite substituir as iagens dos elementos de imagem quando a aplicação foi carregada
         diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
         diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        leftController.isOn = false
+        rightController.isOn = false
+        allController.isOn = true
         
         //diceImageView1.alpha = 0.4
     }
-
-
+    
+    public func controlRollButton() {
+        if (self.leftController.isOn) ||
+            (self.rightController.isOn) ||
+            (self.allController.isOn) {
+            rollButton.isEnabled = true
+        } else {
+            rollButton.isEnabled = false
+        }
+    }
+    
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
+    
+    @IBAction func leftControllerPressed(_ sender: UISwitch) {
+        if self.leftController.isOn {
+            rightController.isOn = false
+            allController.isOn = false
+            diceImageView2.alpha = 0.5
+            diceImageView1.alpha = 1
+        } else {
+            diceImageView1.alpha = 0.5
+        }
+        controlRollButton()
+    }
+    
+    
+    @IBAction func rightControllerPressed(_ sender: UISwitch) {
+        if self.rightController.isOn {
+            leftController.isOn = false
+            allController.isOn = false
+            diceImageView1.alpha = 0.5
+            diceImageView2.alpha = 1
+        } else {
+            diceImageView2.alpha = 0.5
+        }
+        controlRollButton()
+    }
+    
+    
+    @IBAction func allControllerPressed(_ sender: UISwitch) {
+        if self.allController.isOn {
+            rightController.isOn = false
+            leftController.isOn = false
+            diceImageView1.alpha = 1
+            diceImageView2.alpha = 1
+        } else {
+            diceImageView1.alpha = 0.5
+            diceImageView2.alpha = 0.5
+        }
+        controlRollButton()
+    }
+    
 }
 
